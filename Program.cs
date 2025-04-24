1f3579544f81da496389705027f3ade836acd541
Your Name <you@example.com>
2025-04-24 19:45:38 +0800

    4/24

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static int Fibonacci(int n)
+    {
+        if (n <= 1) return n;
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    static void Main()
+    {
+        Console.Write("請輸入第 N 個費式數列項目: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"第 {n} 項費式數列值為: {Fibonacci(n)}");
+    }
+}
